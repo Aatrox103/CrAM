4b54c2109f72221ab97ac2bf881298624008ad91
Aatrox1024 <81019858+Aatrox103@users.noreply.github.com>
2025-10-12 18:03:25 +0800

    Create find_best_heads.py

---
+++ find_best_heads.py
@@ -0,0 +1,17 @@
+import os
+import fire
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+from utils.find_best_heads import *
+
+if __name__ == "__main__":
+    # calculating prob changes when changing different heads
+    casual_tracing_per_head(
+        filepath="nq_100_bge.json",
+        LLM="YOUR_OWN_MODEL_PATH/Meta-Llama-3-8B-Instruct",
+        output_dir="datasets/nq",
+    )
+    # calculating the mean prob change of each head across all data, and sort the results accordingly
+    casual_tracing_combine_all(input_path="datasets/nq/llama3")
+    
+    find_top_k_heads(input_path="datasets/nq/llama3", topk=100)
